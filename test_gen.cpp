f51b58e0f8c16cfa395c2c6a105c58453ac93152
Won-Wook Hong <jradoo@gmail.com>
2020-02-23 18:46:28 +0900

    fix TYPED_TEST_CASE

--- test_gen.cpp
+++ test_gen.cpp
@@ -28,7 +28,7 @@ struct NumericTest : public testing::Test
 
 using NumericTypes = testing::Types<int8_t, int16_t, int32_t, int64_t, uint8_t, uint16_t, uint32_t, uint64_t, float, double>;
 
-TYPED_TEST_SUITE(NumericTest, NumericTypes);
+TYPED_TEST_CASE(NumericTest, NumericTypes);
 
 template <typename T>
 T abs(T& t) {
