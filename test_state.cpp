b9826def47ae4a28d68dd04273966afc714d3706
Won-Wook Hong <jradoo@gmail.com>
2020-03-21 14:37:56 +0900

    working on stateful test

---
+++ test_state.cpp
@@ -0,0 +1,72 @@
+#include "testing/proptest.hpp"
+#include "googletest/googletest/include/gtest/gtest.h"
+#include "googletest/googlemock/include/gmock/gmock.h"
+#include "testing/Random.hpp"
+#include <chrono>
+#include <iostream>
+
+using namespace PropertyBasedTesting;
+
+class StateTestCase : public ::testing::Test {
+};
+
+
+struct Action {
+    virtual bool precondition() {
+        return true;
+    }
+
+    virtual bool run() {
+        return true;
+    };
+};
+
+struct PushBack : public Action {
+    virtual bool run() {
+        std::cout << "PushBack" << std::endl;
+        return true;
+    }
+};
+
+struct Clear : public Action {
+    virtual bool run() {
+        std::cout << "Clear" << std::endl;
+        return true;
+    }
+};
+
+struct Remove : public Action {
+    virtual bool run() {
+        std::cout << "Remove" << std::endl;
+        return true;
+    }
+};
+
+
+
+template <typename T, typename... ARGS>
+std::shared_ptr<T> action(ARGS&&...args) {
+    return std::make_shared<T>(std::move(args)...);
+}
+
+TEST(StateTest, States) {
+   auto actionGen = oneOf<std::shared_ptr<Action>>(
+        [](Random& rand) {
+            return action<PushBack>();
+        },
+        [](Random& rand) {
+            return action<Remove>();
+        },
+        [](Random& rand) {
+            return action<Clear>();
+        }
+    );
+
+    int64_t seed = getCurrentTime();
+    Random rand(seed);
+
+    for(int i = 0; i < 100; i++) {
+        actionGen(rand)->run();
+    }
+
+}
