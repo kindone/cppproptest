ff63bc86a79900740385116e21920182323e4555
Won-Wook Hong <jradoo@gmail.com>
2020-04-27 22:04:59 +0900

    improve safeCall/fork api

---
+++ fork.hpp
@@ -0,0 +1,142 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "api.hpp"
+#include <sys/wait.h>
+#include <stdexcept>
+#include <functional>
+#include <iostream>
+
+namespace PropertyBasedTesting {
+namespace util {
+
+template <typename RET>
+struct PipeResult
+{
+    PipeResult() : success(false), size(0) {}
+    PipeResult(const RET& ret) : success(true), size(sizeof(ret)), ret(ret) {}
+    PipeResult(const std::string& error) : success(false), size(error.size()), msg(error) {}
+
+    void* data()
+    {
+        if (success)
+            return &ret;
+        else
+            return msg.c_str();
+    }
+
+    size_t dataLength() { return size; }
+
+    bool success;
+    size_t size;
+    RET ret;
+    std::string msg;
+};
+
+struct PROPTEST_API Pipe
+{
+    Pipe();
+    ~Pipe();
+    size_t read(void* buf, size_t size);
+    size_t write(void* buf, size_t size);
+
+    template <typename RET>
+    size_t read(PipeResult<RET>& result)
+    {
+        read(&result.success, sizeof(result.success));
+        read(&result.size, sizeof(result.size));
+        if (result.success) {
+            read(&result.ret, sizeof(result.ret));
+        } else {
+            char buf[100];
+            read(buf, result.size);
+            result.msg = std::string(buf, result.size);
+        }
+
+        return sizeof(result.success) + result.size;
+    }
+    template <typename RET>
+    size_t write(PipeResult<RET>& result)
+    {
+        write(&result.success, sizeof(result.success));
+        write(&result.size, sizeof(result.size));
+        if (result.success) {
+            write(&result.ret, sizeof(result.ret));
+        } else
+            write(const_cast<char*>(result.msg.c_str()), result.msg.size());
+
+        return sizeof(result.success) + result.size;
+    }
+
+    void close();
+
+private:
+    int fd[2];
+};
+
+struct PROPTEST_API Fork
+{
+    Fork();
+
+    bool isParent() const { return pid > 0; }
+    bool isChild() const { return pid == 0; }
+    pid_t getPid() const { return pid; }
+
+    void exitNormal();
+    void exitAbnormal();
+
+private:
+    pid_t pid;
+};
+
+template <typename RET>
+RET safeCall(std::function<RET()> func)
+{
+    // prepare pipe for communication
+    Pipe pipe;
+
+    // fork here
+    Fork forked;
+
+    if (forked.isChild()) {
+        try {
+            RET result = func();
+            PipeResult<RET> pipeResult(result);
+            pipe.write<RET>(pipeResult);
+            pipe.close();
+            forked.exitNormal();
+            return result;  // unreachable
+        } catch (std::exception& e) {
+            PipeResult<RET> pipeResult(std::string(e.what()));
+            pipe.write<RET>(pipeResult);
+            pipe.close();
+            forked.exitAbnormal();
+            throw;  // unreachable
+        }
+    }
+    // parent
+    else {
+        int state = 0;
+        pid_t got_pid = waitpid(forked.getPid(), &state, 0);
+        RET result;
+        std::cout << "got pid: " << got_pid << ", exited: " << WIFEXITED(state)
+                  << ", exitstatus: " << WEXITSTATUS(state) << ", signaled: " << WIFSIGNALED(state)
+                  << ", stopped: " << WIFSTOPPED(state) << ", termsig: " << WTERMSIG(state) << std::endl;
+        if (WIFEXITED(state) == 0 /* || WEXITSTATUS(state) != 0*/) {
+            // std::cerr << "forked process ended with error: state = " << state << std::endl;
+            throw std::runtime_error("forked process ended with error: state = " + std::to_string(state));
+        } else {
+            PipeResult<RET> pipeResult;
+            pipe.read(pipeResult);
+            if (pipeResult.success)
+                return pipeResult.ret;
+            else
+                throw std::runtime_error("forked process has thrown an exception: " + pipeResult.msg);
+        }
+    }
+}
+
+}  // namespace util
+}  // namespace PropertyBasedTesting
--- test/test_fork.cpp
+++ test/test_fork.cpp
@@ -1,101 +1,36 @@
 #include "googletest/googletest/include/gtest/gtest.h"
 #include "googletest/googlemock/include/gmock/gmock.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <stdexcept>
+#include "fork.hpp"
 
 class ForkTestCase : public ::testing::Test {
 };
 
-struct A {
+struct A
+{
     int a;
     int b;
 };
 
-template <typename RET>
-RET safeCall(std::function<RET()> func) {
-    // prepare pipe for communication
-    int ctop[2];
-    if (pipe(ctop)==-1)
-        throw std::runtime_error("cannot initialize pipe");
+using namespace PropertyBasedTesting;
 
-    // fork here
-    pid_t pid = fork();
-
-    // fork failed
-    if (pid < 0)
-    {
-        throw std::runtime_error("unable to fork");
-    }
-    // child
-    else if (pid == 0)
-    {
-        try {
-            auto result = func();
-            write(ctop[1], &result, sizeof(result));
-            close(ctop[0]);
-            close(ctop[1]);
-            exit(0);
-            return result;
-        }
-        catch(...) {
-            close(ctop[0]);
-            close(ctop[1]);
-            exit(-1);
-        }
-    }
-    // parent
-    else
-    {
-        int state = 0;
-        pid_t got_pid = waitpid(pid, &state, 0);
-        RET result;
-        printf("WIFEXITED: %d\n", WIFEXITED(state));  // 3
-        printf("WEXITSTATUS: %d\n", WEXITSTATUS(state)); // 4
-        printf("WIFSIGNALED: %d\n", WIFSIGNALED(state)); // 5
-        printf("WIFSTOPPED: %d\n", WIFSTOPPED(state)); // 6
-        printf("WTERMSIG: %d\n", WTERMSIG(state));
-        if(WIFEXITED(state) == 0 || WEXITSTATUS(state) != 0) {
-            std::cerr << "forked process ended with error" << std::endl;
-            close(ctop[1]);
-            close(ctop[0]);
-            throw std::runtime_error("forked process ended with error");
-        }
-        else {
-            std::cout << "reading:" << std::endl;
-            read(ctop[0], &result, sizeof(result));
-        }
-        close(ctop[1]);
-        close(ctop[0]);
-        std::cout << "forked process ended: " << state << std::endl;
-        return result;
-    }
-}
-
-// template <typename RET, typename ...ARGS>
-// RET safeCall(std::function<RET(ARGS...)> func, ARGS&&...args) {
-
-// }
-
-TEST(ForkTestCase, SafeCall1) {
-    auto  result = safeCall<int>([]() {
-        return 5;
-    });
+TEST(ForkTestCase, SafeCall1)
+{
+    auto result = util::safeCall<int>([]() { return 5; });
 
     EXPECT_EQ(result, 5);
 }
 
-TEST(ForkTestCase, SafeCall2) {
-    EXPECT_ANY_THROW(safeCall<int>([]() {
+TEST(ForkTestCase, SafeCall2)
+{
+    EXPECT_ANY_THROW(util::safeCall<int>([]() {
         throw std::runtime_error("error!");
         return 4;
     }));
 }
 
-TEST(ForkTestCase, SafeCall3) {
-    EXPECT_ANY_THROW(safeCall<int>([]() {
+TEST(ForkTestCase, SafeCall3)
+{
+    EXPECT_ANY_THROW(util::safeCall<int>([]() {
         int* a = nullptr;
         *a = 5;
         return *a;
@@ -104,26 +39,23 @@ TEST(ForkTestCase, SafeCall3) {
 
 TEST(ForkTestCase, Fork)
 {
-    printf("--beginning of program--\n");
+    printf("--beginning of test--\n");
     int counter = 0;
     int ptoc[2];
     int ctop[2];
 
-    if (pipe(ptoc)==-1)
-    {
-        fprintf(stderr, "Pipe Failed" );
+    if (pipe(ptoc) == -1) {
+        fprintf(stderr, "Pipe Failed");
         exit(1);
     }
-    if (pipe(ctop)==-1)
-    {
-        fprintf(stderr, "Pipe Failed" );
+    if (pipe(ctop) == -1) {
+        fprintf(stderr, "Pipe Failed");
         exit(1);
     }
 
     pid_t pid = fork();
 
-    if (pid == 0)
-    {
+    if (pid == 0) {
         // child process
         close(ptoc[1]);
         close(ctop[0]);
@@ -132,18 +64,16 @@ TEST(ForkTestCase, Fork)
 
         char message_from_parent[100];
         int len = read(ptoc[0], message_from_parent, 100);
-                if(len > 0)
+        if (len > 0)
             printf("message from parent: %s\n", message_from_parent);
         close(ptoc[0]);
 
         write(ctop[1], "hello from child", 20);
 
-        int *a = nullptr;
-        printf("a: %d\n", *a);//kill
+        int* a = nullptr;
+        printf("a: %d\n", *a);  // kill
         close(ctop[1]);
-    }
-    else if (pid > 0)
-    {
+    } else if (pid > 0) {
         // parent process
         close(ptoc[0]);
         close(ctop[1]);
@@ -155,26 +85,21 @@ TEST(ForkTestCase, Fork)
 
         int state = 0;
         pid_t got_pid = waitpid(pid, &state, 0);
-        printf("got_pid=%d\n",  got_pid);   // 2
-        printf("WIFEXITED: %d\n", WIFEXITED(state));  // 3
-        printf("WEXITSTATUS: %d\n", WEXITSTATUS(state)); // 4
+        printf("got_pid=%d\n", got_pid);                  // 2
+        printf("WIFEXITED: %d\n", WIFEXITED(state));      // 3
+        printf("WEXITSTATUS: %d\n", WEXITSTATUS(state));  // 4
         printf("Done from parent\n");
 
         char message_from_child[100];
         int len = read(ctop[0], message_from_child, 100);
-        if(len > 0)
+        if (len > 0)
             printf("message from child: %s\n", message_from_child);
         close(ctop[0]);
 
-    }
-    else
-    {
+    } else {
         // fork failed
-                printf("fork() failed!\n");
-        exit(1);
+        printf("fork() failed!\n");
     }
 
-    printf("--end of program--\n");
-
-    exit(0);
+    printf("--end of test--\n");
 }
